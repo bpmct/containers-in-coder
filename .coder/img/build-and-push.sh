0dc1e070d486604a81991901f912dff1bfa2c861
Ben Potter <me@bpmct.net>
2021-04-28 17:53:28 -0400

    switch to stable tag and improve versions

--- .coder/img/build-and-push.sh
+++ .coder/img/build-and-push.sh
@@ -1,5 +1,5 @@
 # A helper script for Ben (GitHub actions will do this on every push anyways)
 
 cd $(git rev-parse --show-toplevel)/.coder/img
-docker build . -t bencdr/containers
-docker push bencdr/containers
+docker build . -t bencdr/containers:stable
+docker push bencdr/containers:stable
